c77d6382b5fc8b90ceb27394aae88142f5953531
Teemu Ollakka <teemu.ollakka@codership.com>
2013-11-02 18:41:19 +0200

    References lp:1247402 - bumped interface version to 25

--- wsrep_api.h
+++ wsrep_api.h
@@ -63,7 +63,7 @@ extern "C" {
  *                                                                        *
  **************************************************************************/
 
-#define WSREP_INTERFACE_VERSION "24"
+#define WSREP_INTERFACE_VERSION "25"
 
 /*! Empty backend spec */
 #define WSREP_NONE "none"
